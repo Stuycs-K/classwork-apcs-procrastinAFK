43e71f01eb676c014d5642bca18cbf7bb136bfcb
Maya Berchin <mayab97@nycstudents.net>
2024-09-29 16:17:07 -0400

    Added and tested working returnCopy()

--- 2024-09-27/myArrays.java
+++ 2024-09-27/myArrays.java
@@ -1,18 +1,35 @@
 public class myArrays {
+
   public static void main(String[] args) {
     int[] test = {0,91,76,8235};
-    System.out.println(test);
+    System.out.println("Original array: " + aryToString(test));
+    int[] newTest = returnCopy(test);
+    System.out.println("Copied array: " + aryToString(newTest));
+    System.out.println("Same array ID: " + (test == newTest));
+    test[0] = 9999999;
+    System.out.println("Just in case, changed original array to: " + aryToString(test));
+    System.out.println("The copied array is now: " + aryToString(newTest));
   }
+
   public static String aryToString(int[] nums) {
-    String final = "[";
+    String fin = "[";
     for (int i = 0; i < nums.length; i++) {
-      final += nums[i] + "";
+      fin += nums[i] + "";
       if (i < nums.length - 1) {
-        final += ", ";
+        fin += ", ";
       }
     }
-    return final;
+    return fin + "]";
   }
-  //public static int[] returnCopy(int[] ary) {}
+
+  public static int[] returnCopy(int[] ary) {
+    int[] newAry = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      newAry[i] = ary[i];
+    }
+    return newAry;
+  }
+
   //public static int[] concatArray(int[] ary1, int[] ary2) {}
+
 }
