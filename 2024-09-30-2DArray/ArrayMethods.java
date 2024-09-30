a76eccaf325ad12c41c7845aaf1f126876de84bf
Maya Berchin <mayab97@nycstudents.net>
2024-09-30 19:20:13 -0400

    added and tested a working arr2DSum()

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -5,20 +5,35 @@
 public class ArrayMethods {
 
   public static void main(String[] args) {
-    String arrow = "   ^--> "; //formatting
+    String arrow = "  ^--> "; //formatting
     //test aryToString() (1D arrays)
     System.out.println('\n' + "TESTING ARYTOSTRING() -- 1D ARRAYS");
     System.out.println("*Note* This should already be working, just testing to double check");
     int[] test1 = {1, 2, 3, 4};
     System.out.println(arrow + "ID: " + test1 + "; CONTENTS: " + aryToString(test1));
-    System.out.println("*Note* This function will cause an error if the parameter is a non-initialized array (null)");
+    System.out.println("*Note* This function will cause an error if the parameter is a non-initialized array (contains null)");
     //test arrToString() (2D arrays)
     System.out.println('\n' + "TESTING ARRTOSTRING() -- 2D ARRAYS");
     int[][] test2a = new int[3][4];
     System.out.println(arrow + "Default rectangular array contents: " + arrToString(test2a));
     int[][] test2b = {{0, 1, 2}, {10, 20, 30}, {}, {123, 1234, 12345, 123456, 1234567, 12345678, 123456789}};
     System.out.println(arrow + "Custom non-rectangular array contents: " + arrToString(test2b));
-    System.out.println("*Note* This function calls the one above (aryToString()), so null will once again return an error.");
+	int[][] test2c = new int[2][0];
+	System.out.println(arrow + "Array with two rows and size zero columns: " + arrToString(test2c));
+    System.out.println("*Note* This function calls the one above (aryToString()), so null will once again cause an error.");
+	//test arr2DSum()
+	System.out.println('\n' + "TESTING ARR2DSUM():");
+	int[][] test3a = {{0, 1, 2}, {10}, {}, {20, 7, -40}};
+	System.out.println(arrow + "Array of different lengths; includes an empty array and a negative number");
+	System.out.println("  |    " + arrow + "Array contents: " + arrToString(test3a));
+	System.out.println("  |    " + arrow + "Should sum to: 0");
+	System.out.println("  |    " + arrow + "Sum from function: " + arr2DSum(test3a));
+	test3a = new int[2][3];
+	System.out.println(arrow + "Default 2 x 3 array; should sum to 0; sums to: " + arr2DSum(test3a));
+	int[][] test3b = {{}};
+	System.out.println(arrow + "2D array of length one; its only item is empty; should return 0; returns: " + arr2DSum(test3b));
+	int[][] test3d = {{1, 2, 3}, {2, 2}};
+	System.out.println(arrow + "Array contents: " + arrToString(test3d) + "; sums to: " + arr2DSum(test3d));
   }
 
 //2. Copy your aryToString method from before.
@@ -60,7 +75,13 @@ public class ArrayMethods {
 /*Return the sum of all of the values in the 2D array */
   public static int arr2DSum(int[][]nums){
     //use a nested loop to solve this
-    return 0; //temporary
+    int sum = 0;
+	for (int i = 0; i < nums.length; i++) { //loops through the first dimension of the 2D array
+	  for (int j = 0; j < nums[i].length; j++) { //loops through the second dimension of the 2D array
+	    sum += nums[i][j];
+	  }
+	}
+	return sum;
   }
 
 /**Rotate an array by returning a new array with the rows and columns swapped.
