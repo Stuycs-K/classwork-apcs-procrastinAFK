fd3b4a869248a69240fd7547e1115c2882616af2
Maya Berchin <mayab97@nycstudents.net>
2024-09-30 20:31:10 -0400

    EVERYTHING WORKS; added and tested swapRC()

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -29,11 +29,36 @@ public class ArrayMethods {
 	System.out.println("  |    " + arrow + "Should sum to: 0");
 	System.out.println("  |    " + arrow + "Sum from function: " + arr2DSum(test3a));
 	test3a = new int[2][3];
-	System.out.println(arrow + "Default 2 x 3 array; should sum to 0; sums to: " + arr2DSum(test3a));
+	System.out.println(arrow + "Default 2x3 array; should sum to 0; sums to: " + arr2DSum(test3a));
 	int[][] test3b = {{}};
 	System.out.println(arrow + "2D array of length one; its only item is empty; should return 0; returns: " + arr2DSum(test3b));
 	int[][] test3d = {{1, 2, 3}, {2, 2}};
 	System.out.println(arrow + "Array contents: " + arrToString(test3d) + "; sums to: " + arr2DSum(test3d));
+	int[][] test3e = {{-1, -2, -3}, {-4}};
+	System.out.println(arrow + "Array contents: " + arrToString(test3e) + "; sums to: " + arr2DSum(test3e));
+	//test swapRC()
+	System.out.println('\n' + "TESTING SWAPRC():");
+	int[][] test4a = new int[3][4];
+	System.out.println(arrow + "Change a 3x4 default array into a 4x3 default array:");
+	System.out.println("  |    " + arrow + "Original array: " + arrToString(test4a));
+	test4a = swapRC(test4a);
+	System.out.println("  |    " + arrow + "Swapped array: " + arrToString(test4a));
+	test4a = swapRC(test4a);
+	System.out.println("  |    " + arrow + "Swapped back: " + arrToString(test4a));
+	int[][] test4b = {{0, 1, 2}, {0, 1, 2}};
+	System.out.println(arrow + "Change a 2x3 custom array into a 3x2 custom array:");
+	System.out.println("  |    " + arrow + "Original array: " + arrToString(test4b));
+	test4b = swapRC(test4b);
+	System.out.println("  |    " + arrow + "Swapped array: " + arrToString(test4b));
+	test4b = swapRC(test4b);
+	System.out.println("  |    " + arrow + "Swapped back: " + arrToString(test4b));
+	int[][] test4c = {{0, 1, 2, 3, 4, 5}};
+	System.out.println(arrow + "Change a 1x5 custom array into a 5x1 custom array:");
+	System.out.println("  |    " + arrow + "Original array: " + arrToString(test4c));
+	test4c = swapRC(test4c);
+	System.out.println("  |    " + arrow + "Swapped array: " + arrToString(test4c));
+	test4c = swapRC(test4c);
+	System.out.println("  |    " + arrow + "Swapped back: " + arrToString(test4c));
   }
 
 //2. Copy your aryToString method from before.
@@ -73,7 +98,7 @@ public class ArrayMethods {
   }
 
 /*Return the sum of all of the values in the 2D array */
-  public static int arr2DSum(int[][]nums){
+  public static int arr2DSum(int[][]nums) {
     //use a nested loop to solve this
     int sum = 0;
 	for (int i = 0; i < nums.length; i++) { //loops through the first dimension of the 2D array
@@ -88,8 +113,14 @@ public class ArrayMethods {
   * You may assume the array is rectangular and neither rows nor cols is 0.
   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   */
-  public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
+  public static int[][] swapRC(int[][]nums) {
+	int[][] swap = new int[nums[0].length][nums.length];
+	for (int i = 0; i < swap.length; i++) {
+		for (int j = 0; j < swap[i].length; j++) {
+			swap[i][j] = nums[j][i];
+		}
+	}
+	return swap;
   }
 
 }
