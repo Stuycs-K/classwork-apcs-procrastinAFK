ee5d6ed8714a24e25a63402a8add1f533d84a657
Maya Berchin <mayab97@nycstudents.net>
2024-11-04 10:56:38 -0500

    Fixed and tested isTraianlge (more testing needed)

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,3 +1,8 @@
+//1 you need all three imports.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriangleTester {
 
   public static int countTrianglesA(String filename) {
@@ -5,13 +10,19 @@ public class TriangleTester {
     return 0; //so it can compile
   }
 
-  public static boolean isTriangle(ArrayList<Integer> sides) {
+  public static boolean isTriangle(int[] sides) {
     boolean isTri = true;
-    for (int i = 0; i < sizes.size() && isTri; i++) {
-      if (sizes.get(i) + sides.get(i + 1) <= sides.get(i + 2)) {
+    for (int i = 0; i < sides.length && isTri; i++) {
+      if (sides[0] + sides[1] <= sides[2]) {
         isTri = false;
       }
+      sides = new int[]{sides[1], sides[2], sides[0]};
     }
     return isTri;
+  }
+
+  public static void main(String[] args) {
+    int[] teststuff = new int[]{5, 10, 25};
+    System.out.println("5, 10, 25 (should be FALSE): " + isTriangle(teststuff));
   }
 }
