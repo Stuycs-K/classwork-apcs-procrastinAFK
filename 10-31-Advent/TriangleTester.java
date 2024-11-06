e37cbf7d2f3df2c522f48a8ee0cf3b221abbcc1e
Maya Berchin <mayab97@nycstudents.net>
2024-11-05 20:50:58 -0500

    Added countTrianglesB, no testing yet

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class TriangleTester {
 
-  public static int countTrianglesA(String filename) {
+  public static int countTrianglesA(String filename) { //new triangle per \n, sides split by spaces
     int count = 0;
 	try {
       File file = new File(filename);
@@ -13,13 +13,42 @@ public class TriangleTester {
 	  while (input.hasNextLine()) {
 	    String line = input.nextLine();
 	    String[] nums = line.split(" ");
-	    int[] sides = new int[nums.length];
-	    for (int i = 0; i < nums.length; i++) {
+	    int[] sides = new int[3]; //sides of a triangle
+	    for (int i = 0; i < sides.length; i++) {
 		  sides[i] = Integer.parseInt(nums[i]);
 	    }
 	    if (isTriangle(sides)) {
 		  count++;
 	    }
+	  }
+	  input.close();
+	} catch(FileNotFoundException ex) {
+	  System.out.println("File not found!");
+	}
+    return count;
+  }
+  
+  public static int countTrianglesB(String filename) { //new triangle per space, sides split by \n
+	int count = 0;
+	try {
+      File file = new File(filename);
+	  Scanner input = new Scanner(file);
+	  while (input.hasNextLine()) { //actually means it has 3 or more lines since we're allowed to assume there is a multiple of 3 number of lines
+	    int[][] allSides = new int[3][3]; //3 triangles, 3 sides per triangle
+		for (int sideNum = 0; sideNum < allSides.length; sideNum++) { //make a 2D array with all the side lengths of each triangle
+		  String[] nums = input.nextLine().split(" ");
+		  int[] sides = new int[3]; //side number sideNum of each triangle
+	      for (int i = 0; i < sides.length; i++) {
+		    sides[i] = Integer.parseInt(nums[i]);
+	      }
+		  allSides[sideNum] = sides;
+		}
+		for (int i = 0; i < 3; i++) { //for each triangle
+		  int[] triSides = new int[]{allSides[0][i], allSides[1][i], allSides[2][i]};
+	      if (isTriangle(triSides)) {
+		    count++;
+	      }
+		}
 	  }
 	  input.close();
 	} catch(FileNotFoundException ex) {
