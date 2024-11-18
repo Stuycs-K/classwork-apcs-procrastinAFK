cc45ad1b5c460a2d9387c331dbfc94df46689991
Maya Berchin <mayab97@nycstudents.net>
2024-11-17 19:21:46 -0500

    Added mixLists

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -32,5 +32,23 @@ public class ArrayListPractice{
     }
     return revArr;
   }
+  
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+    //return a new ArrayList that has all values of a and b in alternating order that is:
+    //a[0], b[0], a[1], b[1]...
+    //If one list is longer than the other, just attach the remaining values to the end.
+	ArrayList<String> finList = new ArrayList<String>(a.size() + b.size());
+	for (int i = 0; i < a.size() && i < b.size(); i++) {
+	  finList.add(a.get(i));
+	  finList.add(b.get(i));
+	}
+	for (int i = 0; i < a.size(); i++) { //executes if a is larger than b and therefore isn't size 0 by now
+	  finList.add(a.get(i));
+	}
+	for (int i = 0; i < b.size(); i++) { //executes if b is larger than a and therefore isn't size 0 by now
+	  finList.add(b.get(i));
+	}
+	
+  } 
 
 }
