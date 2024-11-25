66c680b6470e4446f45c1f3b5a5e8ab53f4c992d
Maya Berchin <mayab97@nycstudents.net>
2024-11-24 20:42:58 -0500

    Finished part 2 of day 1

--- 2024-11-18-advent2016/AdventRunner.java
+++ 2024-11-18-advent2016/AdventRunner.java
@@ -1,10 +1,9 @@
 public class AdventRunner {
 	public static void main(String[] args) {
-		System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
-		System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
+		System.out.println("Number of valid triangles by row: " + TriangleTester.countTrianglesA("inputTri.txt"));
+		System.out.println("Number of valid triangles by column (3 nums at a time): " + TriangleTester.countTrianglesB("inputTri.txt"));
 		
 		System.out.println("\n");
-		System.out.println(fileParser.parseFile("inputDay1.txt"));
 		System.out.println(Day1.blocksAway("inputDay1.txt"));
 	}
 }
--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -6,15 +6,32 @@ import java.util.ArrayList;
 public class Day1 {
 
 	public static int blocksAway(String filename) { //filename = name of input file
+	
 		int distance = 0;
 		String dir = "N";
 		ArrayList<String> directions = fileParser.parseFile(filename);
+		
+		ArrayList<String> visited = new ArrayList<String>();
+		int[] curCoords = new int[]{0,0};
+		visited.add(coords(curCoords));
+		boolean samespot = false;
+		
 		for (int i = 0; i < directions.size(); i++) {
+			
+			//distance calc stuff
 			String thisDir = directions.get(i);
 			String turn = thisDir.substring(0,1); //first char = direction to turn
 			dir = changeDir(dir, turn);
 			int blocks = Integer.parseInt(thisDir.substring(1));
 			distance += moveBlocks(dir, blocks);
+			
+			//visited before stuff
+			if (! samespot) {
+				visited = newVisiteds(visited, curCoords, dir, blocks);
+				if (visited.size() == 1) {
+					samespot = true;
+				}
+			}
 		}
 		return Math.abs(distance);
 	}
@@ -36,6 +53,33 @@ public class Day1 {
 			sign = -1;
 		}
 		return num * sign;
+	}
+	
+	
+	public static String coords(int[] curCoords) {
+		return "(" + curCoords[0] + "," + curCoords[1] + ")";
+	}
+	
+	
+	public static ArrayList newVisiteds(ArrayList<String> visited, int[] curCoords, String dir, int blocks) {
+		blocks = moveBlocks(dir, blocks);
+		int increment = blocks / Math.abs(blocks); //decides if the increment should be -1 or 1
+		int coordInd = 0;
+		if (dir.equals("N") || dir.equals("S")) {
+			coordInd = 1;
+		}
+		for (int i = 0; i < Math.abs(blocks); i++) {
+			curCoords[coordInd] += increment;
+			String strCoords = coords(curCoords);
+			if (visited.contains(strCoords)) {
+				System.out.println("First spot visited twice is this many blocks away: " + (Math.abs(curCoords[0]) + Math.abs(curCoords[1])) + " at " + strCoords);
+				visited = new ArrayList<String>();
+				visited.add(strCoords);
+				return visited;
+			}
+			visited.add(strCoords);
+		}
+		return visited;
 	}
 
 }
