13d52d18b7b2897f68c97b3c12c5118c58d182e0
Maya Berchin <mayab97@nycstudents.net>
2024-12-08 21:13:36 -0500

    Changed using Mage variable to using Adventurer variable (still using Mage instantiation)

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -2,7 +2,7 @@ public class Driver {
   public static void main(String[] args) {
 	  
 	System.out.println("\nTESTING BASIC MAGE:");
-    Mage p1 = new Mage("Aiura");
+    Adventurer p1 = new Mage("Aiura");
 	System.out.println("toString: " + p1);
 	System.out.println("Name (param = \"Aiura\"): " + p1.getName());
 	System.out.println("Max HP (no param): " + p1.getmaxHP());
@@ -23,7 +23,7 @@ public class Driver {
 	System.out.println("Max special (param = 15): " + p1.getSpecialMax());
 	
 	System.out.println("\nTESTING METHODS: ");
-	Mage p2 = new Mage("Sancha");
+	Adventurer p2 = new Mage("Sancha");
 	System.out.println("p1: " + p1 + "; p2: " + p2);
 	System.out.println("These mages both should have the same special ability!");
 	System.out.println("  - " + p1 + ": " + p1.getSpecialName());
