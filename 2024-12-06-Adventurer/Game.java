468dd1ebb4faf3f33ec6ec5e3e57725e205ccae5
Maya Berchin <mayab97@nycstudents.net>
2024-12-09 10:58:14 -0500

    Added a printStats helper function and created p1 and enemy (adventurers)

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -8,6 +8,16 @@ public class Game {
     System.out.println("Welcome! Please type a username.");
     String username = userInput.nextLine();
     System.out.println("Welcome, " + username + "!");
+
+    //Create p1 and opponent
+    Adventurer p1 = new Mage(username);
+    System.out.println("Your character: " + printStats(p1));
+    Adventurer enemy = new CodeWarrior();
+    System.out.println("Your opponent: " + printStats(enemy));
+  }
+
+  public static String printStats(Adventurer p) {
+    return p + ", " + p.getHP() + "/" + p.getmaxHP() + " HP, " + p.getSpecial() + "/" + p.getSpecialMax() + " " + p.getSpecialName();
   }
 
   public static void enemyMove(Adventurer enemy, Adventurer other) {
@@ -22,5 +32,4 @@ public class Game {
       enemy.specialAttack(other);
     }
   }
-
 }
